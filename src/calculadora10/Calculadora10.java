68f9912c097ea84d52488529d2db3274e2396572
Iker <iker.fernandez-06@somo.eus>
2026-02-11 11:02:48 +0100

    Añadidos comentarios de Javadoc

--- src/calculadora10/Calculadora10.java
+++ src/calculadora10/Calculadora10.java
@@ -11,17 +11,49 @@ package calculadora10;
 
 public class Calculadora10 {
 	
+	/**
+	 * Suma dos números enteros.
+	 * 
+	 * @param a primer número
+	 * @param b segundo número
+	 * @return resultado de la suma
+	 */
+	
 	public int sumar(int a, int b) {
 		return a + b;
 	}
 
+	/**
+	 * Resta dos números enteros.
+	 * 
+	 * @param a primer número
+	 * @param b segundo número
+	 * @return resultado de la resta
+	 */
+	
 	public int restar(int a, int b) {
 		return a - b;
 	}
 	
+	/**
+	 * Multiplica dos números enteros.
+	 * 
+	 * @param a primer número
+	 * @param b segundo número
+	 * @return resultado de la multiplicacion
+	 */
+	
 	public int multiplicar(int a, int b) {
 		return a * b;
 	}
+	
+	/**
+	 * Divide dos números enteros.
+	 * 
+	 * @param a primer número
+	 * @param b segundo número
+	 * @return resultado de la division
+	 */
 	
 	public double dividir(int a, int b) {
 		if (b == 0) {
@@ -31,6 +63,14 @@ public class Calculadora10 {
 		return (double) a / b;
 	}
 	
+	/**
+	 * Potencia de dos números enteros.
+	 * 
+	 * @param a primer número
+	 * @param b segundo número
+	 * @return resultado de la potencia
+	 */
+	
 	public int potencia(int base, int exponente) {
 		int resultado = 1;
 		for (int i = 0; i < exponente; i++) {
@@ -38,6 +78,14 @@ public class Calculadora10 {
 		}
 		return resultado;
 	}
+	
+	/**
+	 * Modulo dos números enteros.
+	 * 
+	 * @param a primer número
+	 * @param b segundo número
+	 * @return resultado del modulo
+	 */
 	
 	public int modulo(int a, int b) {
 		return a % b;
