65685c0277e635e710a4c29bad411e8b904dddb3
Iker <iker.fernandez-06@somo.eus>
2026-02-11 09:39:57 +0100

    Proyecto CalculadoraGIT10 añadir control sobre la division

--- src/calculadora10/Calculadora10.java
+++ src/calculadora10/Calculadora10.java
@@ -15,6 +15,9 @@ public class Calculadora10 {
 	}
 	
 	public double dividir(int a, int b) {
+		if (b == 0) {
+		throw new IllegalArgumentException("No se puede dividir entre cero");
+		}
 		return (double) a / b;
 	}
 }
